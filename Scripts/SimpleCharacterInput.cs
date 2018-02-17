f335793197813e990b9ce540216753d4c0037dbe
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-17 13:12:15 +0700

    Add input and result, input will be sent to server to calculate result to all clients

---
+++ Scripts/SimpleCharacterInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLibHighLevel;
+using LiteNetLib.Utils;
+
+public struct SimpleCharacterInput
+{
+    public float horizontal;
+    public float vertical;
+    public bool isJump;
+    public uint tick;
+}
+
+public class SimpleCharacterInputField : LiteNetLibNetField<SimpleCharacterInput>
+{
+    public override void Deserialize(NetDataReader reader)
+    {
+        var result = new SimpleCharacterInput();
+        result.horizontal = (float)reader.GetShort() * 0.01f;
+        result.vertical = (float)reader.GetShort() * 0.01f;
+        result.isJump = reader.GetBool();
+        result.tick = reader.GetUInt();
+        Value = result;
+    }
+
+    public override bool IsValueChanged(SimpleCharacterInput newValue)
+    {
+        return !newValue.Equals(Value);
+    }
+
+    public override void Serialize(NetDataWriter writer)
+    {
+        writer.Put((short)(Value.horizontal * 100));
+        writer.Put((short)(Value.vertical * 100));
+        writer.Put(Value.isJump);
+        writer.Put(Value.tick);
+    }
+}
---
+++ Scripts/SimpleCharacterResult.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiteNetLibHighLevel;
+using LiteNetLib.Utils;
+
+public struct SimpleCharacterResult
+{
+    public Vector3 speed;
+    public Vector3 position;
+    public Quaternion rotation;
+    public uint tick;
+}
+
+public class SimpleCharacterResultField : LiteNetLibNetField<SimpleCharacterResult>
+{
+    public override void Deserialize(NetDataReader reader)
+    {
+        var result = new SimpleCharacterResult();
+        result.speed = new Vector3((float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f);
+        result.position = new Vector3((float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f);
+        result.rotation = new Quaternion((float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f, (float)reader.GetShort() * 0.01f);
+        result.tick = reader.GetUInt();
+        Value = result;
+    }
+
+    public override bool IsValueChanged(SimpleCharacterResult newValue)
+    {
+        return !newValue.Equals(Value);
+    }
+
+    public override void Serialize(NetDataWriter writer)
+    {
+        writer.Put((short)(Value.speed.x * 100));
+        writer.Put((short)(Value.speed.y * 100));
+        writer.Put((short)(Value.speed.z * 100));
+        writer.Put((short)(Value.position.x * 100));
+        writer.Put((short)(Value.position.y * 100));
+        writer.Put((short)(Value.position.z * 100));
+        writer.Put((short)(Value.rotation.x) * 100);
+        writer.Put((short)(Value.rotation.y) * 100);
+        writer.Put((short)(Value.rotation.z) * 100);
+        writer.Put((short)(Value.rotation.w) * 100);
+        writer.Put(Value.tick);
+    }
+}
